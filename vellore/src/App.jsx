d70a6a1b35b2efe4e7a6ba7b3d0d6e5ebaf277e4
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-07-27 11:48:06 +0530

    renamed files

--- vellore/src/App.jsx
+++ vellore/src/App.jsx
@@ -2,17 +2,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="h-screen w-screen overflow-hidden flex items-center justify-center">
-      <video
-        className="w-full h-full object-cover"
-        autoPlay
-        muted
-        loop
-        playsInline 
-      >
-        <source src="/videos/TshirtVideo-vmake.mov" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <div>
+      Nothing
     </div>
   );
 };
