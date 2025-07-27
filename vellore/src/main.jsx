7866dda52757214b79c29537d0d14c4f89f48f2c
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-07-27 11:21:18 +0530

    deleted Videos

--- vellore/src/main.jsx
+++ vellore/src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import queryClient from './services/queryClient.js'
 import { QueryClientProvider } from '@tanstack/react-query';
-import {Provider} from "react-redux"
-import {store} from '../redux/index.js'
+import { Provider } from "react-redux"
+import { store } from '../redux/index.js'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
-    <App />
+      <App />
     </QueryClientProvider>
   </Provider>,
 )
