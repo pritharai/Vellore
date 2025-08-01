ae0c64a0c45e9677f850fc32132365697ad1b43c
Pritha Rai <pritharai873@gmail.com>
2025-08-01 23:14:55 +0530

    Testimonialv1

--- vellore/src/App.jsx
+++ vellore/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Testimonials from './components/Testimonials';
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
      <Navbar/>
       <Home/>
+      <Testimonials/>
     </>
   );
 };
