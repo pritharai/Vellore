2b30106bc5b21d29d94fd11dc0ca17c4a07eb9d7
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-08-01 23:37:27 +0530

    Done

--- vellore/src/App.jsx
+++ vellore/src/App.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-
-import Testimonials from './components/Testimonials';
-
 import ProductDetail from './components/ProductDetails';
-
 
 
 const App = () => {
   return (
     <>
      <Navbar/>
-
-      <Home/>
-      <Testimonials/>
-
      <div>
       {/* <Home/> */}
-      {/* <ProductDetail/> */}
-      {/* <Wishlist/> */}
-      <Cart/>
+      <ProductDetail/>
      </div>
-
     </>
   );
 };
