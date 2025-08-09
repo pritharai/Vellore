59f2ae44cebe3f764678786ddb79b8cde13e838f
Pritha Rai <pritharai873@gmail.com>
2025-08-09 21:52:25 +0530

    product updated

--- vellore/src/data/sampleProducts.jsx
+++ vellore/src/data/sampleProducts.jsx
@@ -13,12 +13,12 @@ const sampleProducts = [
   {
     title: "Visionary Lie",
     price: 2899,
-    image: "/images/hero-core-1.jpg",
+    image: "/images/product3.jpg",
   },
   {
     title: "Outline of a Legend",
     price: 4499,
-    image: "/images/hero-core-2.jpg",
+    image: "/images/product4.jpg",
   },
   
   {
