b6cdb32865c25015c774035988b50d17cf2a1d00
Pritha Rai <pritharai873@gmail.com>
2025-08-19 22:08:50 +0530

    fixed conflict

--- vellore/src/App.jsx
+++ vellore/src/App.jsx
@@ -134,7 +134,7 @@ const App = () => {
                 </>
               }
             />
-
+                <Route
                 path="/search"
                 element={
                   <>
