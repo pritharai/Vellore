150eeb996e866773d0536bd3f5928c03077024c7
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2026-03-08 20:46:26 +0530

    added console check

--- vellore/src/services/orderService.js
+++ vellore/src/services/orderService.js
@@ -3,6 +3,7 @@ import api from "./api";
 export const createOrder = async (data) => {
   try {
     const response = await api.post("/order", data);
+    console.log(response.data.data)
     return response.data.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Failed to create order");
